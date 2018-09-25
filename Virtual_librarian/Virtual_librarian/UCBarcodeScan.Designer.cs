36805ebcb6a96190d26987c9eba0b1e8bc7d2d9a
entorius <eligijus.puplinskas@gmail.com>
2018-09-25 21:09:15 +0300

    barcode scannner added

--- Virtual_librarian/Virtual_librarian/UCBarcodeScan.Designer.cs
+++ Virtual_librarian/Virtual_librarian/UCBarcodeScan.Designer.cs
@@ -29,25 +29,74 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label1.Location = new System.Drawing.Point(333, 188);
+            this.label1.Location = new System.Drawing.Point(116, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(459, 54);
+            this.label1.Size = new System.Drawing.Size(183, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Barkodo skenavimas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Skenuoti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "barkodas";
             // 
             // UCBarcodeScan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "UCBarcodeScan";
-            this.Size = new System.Drawing.Size(1168, 691);
+            this.Size = new System.Drawing.Size(438, 290);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +105,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Virtual_librarian/Virtual_librarian/UCBarcodeScan.cs
+++ Virtual_librarian/Virtual_librarian/UCBarcodeScan.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Spire.Barcode;
 
 namespace Virtual_librarian
 {
     public partial class UCBarcodeScan : UserControl
     {
+        String BarcodeLocation; // String nurodantis failo vietą
+        Image File;             // Kintamasis kuris saugo paveikslėlį   :    vėliau nebereikės kai bus pritaikytas skenavimas kamera
         public UCBarcodeScan()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)     // Mygtuko Skenuoti paspaudimo event
+        {
+            OpenFileDialog open = new OpenFileDialog();           // Atidaro dialog langa atsidaryti nuotrauką iš kompiuterio failų
+            open.Filter = "JPG(*.JPG|*.jpg";                      // Folderiuose filtruoja JPG failus
+
+            if (open.ShowDialog() == DialogResult.OK)           //Event jei mygtukas OK dialog lange buvo paspaustas
+            {
+                textBox1.Clear();
+                File = Image.FromFile(open.FileName);
+                pictureBox1.Image = File;
+                BarcodeLocation = open.FileName;
+                String[] Barcode = BarcodeScanner.Scan(BarcodeLocation);
+               
+                textBox1.AppendText(Barcode[0]);
+                
+            }
         }
     }
 }
