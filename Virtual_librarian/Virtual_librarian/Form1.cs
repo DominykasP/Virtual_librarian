ddcfcbb3c6a6dc5d162d409379514f99dfb0897d
Dominykas <dominykp@gmail.com>
2018-09-25 17:45:51 +0300

    Pajungtas webcam'as. Kamera padaro nustatytą skaičių foto. Pradinis UI veido skenavimui.

--- Virtual_librarian/Virtual_librarian/Form1.cs
+++ Virtual_librarian/Virtual_librarian/Form1.cs
@@ -12,9 +12,39 @@ namespace Virtual_librarian
 {
     public partial class Form1 : Form
     {
+        private UCFaceScan faceScanControl;
+        private UCBarcodeScan barcodeScanControl;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnFace_Click(object sender, EventArgs e)
+        {
+            if(this.Controls.Contains(barcodeScanControl))
+            {
+                this.Controls.Remove(barcodeScanControl);
+            }
+
+            faceScanControl = new UCFaceScan();
+            this.Controls.Add(faceScanControl);
+        }
+
+        private void btnBarcode_Click(object sender, EventArgs e)
+        {
+            if (this.Controls.Contains(faceScanControl))
+            {
+                faceScanControl.tmrForVideo.Stop();
+                faceScanControl.tmrForCapure.Stop();
+                faceScanControl.capture.Stop();
+                faceScanControl.capture.Dispose();
+                faceScanControl.images.Clear();
+                this.Controls.Remove(faceScanControl);
+            }
+
+            barcodeScanControl = new UCBarcodeScan();
+            this.Controls.Add(barcodeScanControl);
         }
     }
 }
---
+++ Virtual_librarian/Virtual_librarian/UCFaceScan.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+
+namespace Virtual_librarian
+{
+    public partial class UCFaceScan : UserControl
+    {
+        public VideoCapture capture;
+
+        private int numberOfSeconds = 3; //Per kiek sekundziu padarysim visas nuotraukas
+        private static int numberOfImages = 5; //Kiek is viso reikes padaryti nuotrauku
+        double intervalForCapture; //Kas kiek milisekundziu reiks daryti nuotrauka
+        public List<Bitmap> images; //Cia bus visos padarytos nuotraukos, kurias veliau siusim i FaceAPI
+
+        public Timer tmrForVideo;
+        public Timer tmrForCapure;
+
+        public UCFaceScan()
+        {
+            InitializeComponent();
+
+            capture = new VideoCapture();
+
+            images = new List<Bitmap>();
+            tmrForVideo = new Timer();
+            tmrForCapure = new Timer();
+            intervalForCapture = Math.Truncate(((double)numberOfSeconds / (double)numberOfImages) * 1000);
+
+            tmrForVideo.Interval = 40; //40 milisekundes - 25 frames per second
+            tmrForCapure.Interval = System.Convert.ToInt32(intervalForCapture); //Dabar 0,6 sekundes
+
+            tmrForVideo.Enabled = true;
+            tmrForCapure.Enabled = true;
+
+            tmrForVideo.Tick += TmrForVideo_Tick;
+            tmrForCapure.Tick += TmrForCapure_Tick;
+
+            pbarScanning.Maximum = numberOfImages;
+        }
+
+        private void TmrForCapure_Tick(object sender, EventArgs e)
+        {
+            if(images.Count == numberOfImages)
+            {
+                //tmrForVideo.Enabled = false; //Dabar video nesustos
+                tmrForCapure.Enabled = false;
+
+                showImages(); //Parodo padarytas nuotraukas - sito veliau nebereikes - testavimo tikslais
+            }
+            else
+            {
+                images.Add(showOneFrame());
+                pbarScanning.Value += pbarScanning.Step;
+            }
+        }
+
+        private void showImages() //Parodo padarytas nuotraukas - sito veliau nebereikes - testavimo tikslais
+        {
+            int kuriRodo = 0;
+
+            Form formaParodymui = new Form();
+
+            PictureBox nuotrauka = new PictureBox();
+            nuotrauka.Height = formaParodymui.Height;
+            nuotrauka.Width = formaParodymui.Width;
+            nuotrauka.SizeMode = PictureBoxSizeMode.StretchImage;
+            nuotrauka.Image = images[kuriRodo];
+
+            Button toliau = new Button();
+            toliau.Text = "Kita foto";
+            toliau.Click += delegate
+            {
+                kuriRodo++;
+                if (kuriRodo == 5)
+                {
+                    kuriRodo = 0;
+                }
+                nuotrauka.Image = images[kuriRodo];
+            };
+
+            formaParodymui.Controls.Add(nuotrauka);
+            formaParodymui.Controls.Add(toliau);
+            nuotrauka.SendToBack();
+
+            formaParodymui.ShowDialog();
+        }
+
+        private void TmrForVideo_Tick(object sender, EventArgs e)
+        {
+            showOneFrame();
+        }
+
+        private Bitmap showOneFrame()
+        {
+            Bitmap image = capture.QueryFrame().Bitmap;
+            picWebCam.Image = image;
+
+            return image;
+        }
+    }
+}
