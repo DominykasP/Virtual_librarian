fde98b5120f56a4e47d30a25910d1464385ca08c
Dominykas <dominykp@gmail.com>
2018-10-03 23:08:49 +0300

    Pataisytas knygu skenavimo dizainas

--- Virtual_librarian/Virtual_librarian/UCScanBook.Designer.cs
+++ Virtual_librarian/Virtual_librarian/UCScanBook.Designer.cs
@@ -35,6 +35,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.BarcodeBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -42,21 +43,21 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(77, 35);
+            this.metroLabel1.Location = new System.Drawing.Point(67, 35);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(109, 19);
+            this.metroLabel1.Size = new System.Drawing.Size(183, 19);
             this.metroLabel1.TabIndex = 0;
-            this.metroLabel1.Text = "Skanuokite knygą";
+            this.metroLabel1.Text = "Nuskanuokite knygos barkodą";
             // 
             // metroTile1
             // 
             this.metroTile1.ActiveControl = null;
-            this.metroTile1.Location = new System.Drawing.Point(34, 183);
+            this.metroTile1.Location = new System.Drawing.Point(24, 67);
             this.metroTile1.Margin = new System.Windows.Forms.Padding(2);
             this.metroTile1.Name = "metroTile1";
-            this.metroTile1.Size = new System.Drawing.Size(124, 50);
+            this.metroTile1.Size = new System.Drawing.Size(226, 41);
             this.metroTile1.TabIndex = 1;
-            this.metroTile1.Text = "Skenuoti Barkodą";
+            this.metroTile1.Text = "Įjungti kamerą";
             this.metroTile1.UseSelectable = true;
             this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
             // 
@@ -65,7 +66,7 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::Virtual_librarian.Properties.Resources.icons8_barcode_64;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(34, 35);
+            this.pictureBox2.Location = new System.Drawing.Point(24, 35);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(38, 28);
@@ -75,12 +76,12 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(266, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(291, 35);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(354, 224);
+            this.pictureBox1.Size = new System.Drawing.Size(342, 197);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -106,7 +107,7 @@
             this.BarcodeBox1.CustomButton.Visible = false;
             this.BarcodeBox1.Enabled = false;
             this.BarcodeBox1.Lines = new string[0];
-            this.BarcodeBox1.Location = new System.Drawing.Point(34, 138);
+            this.BarcodeBox1.Location = new System.Drawing.Point(24, 192);
             this.BarcodeBox1.Margin = new System.Windows.Forms.Padding(2);
             this.BarcodeBox1.MaxLength = 32767;
             this.BarcodeBox1.Name = "BarcodeBox1";
@@ -116,16 +117,26 @@
             this.BarcodeBox1.SelectionLength = 0;
             this.BarcodeBox1.SelectionStart = 0;
             this.BarcodeBox1.ShortcutsEnabled = true;
-            this.BarcodeBox1.Size = new System.Drawing.Size(218, 40);
+            this.BarcodeBox1.Size = new System.Drawing.Size(226, 40);
             this.BarcodeBox1.TabIndex = 4;
             this.BarcodeBox1.UseSelectable = true;
             this.BarcodeBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
             this.BarcodeBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(24, 165);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(127, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Atpažintas barkodas";
+            // 
             // UCScanBook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.BarcodeBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -149,5 +160,6 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private MetroFramework.Controls.MetroTextBox BarcodeBox1;
         public System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
     }
 }
