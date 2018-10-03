7f0d2d3b95960f622bb283d827fb3a30afb24c46
Dominykas <dominykp@gmail.com>
2018-10-03 14:25:40 +0300

    Nauja forma su MetroFramework. Pradinis prisijungimo pasirinkimo langas.

---
+++ Virtual_librarian/Virtual_librarian/DB helpers interfaces/BookDBHelperInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_librarian.DB_helpers
+{
+    interface BookDBHelperInterface
+    {
+        List<Knyga> gautiVisasKnygas();
+        List<Knyga> ieskoti(string search);
+
+        bool pridetiNaujaKnyga(Knyga knyga);
+        bool istrintiKnyga(int knygosID);
+        bool paimtiKnyga(int knygosID, int skaitytojoID);
+        bool grazintiKnyga(int knygosID);
+    }
+}
---
+++ Virtual_librarian/Virtual_librarian/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Virtual_librarian
+{
+    public partial class MainForm : MetroFramework.Forms.MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrisijungti_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistruotis_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
