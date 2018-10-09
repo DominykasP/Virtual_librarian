b94fb80a35e2deb07e1fa180c0c8460d95d778aa
entorius <eligijus.puplinskas@gmail.com>
2018-10-09 14:14:39 +0300

    face recognition Dll and more

--- Virtual_librarian/Virtual_librarian/UCLogin.Designer.cs
+++ Virtual_librarian/Virtual_librarian/UCLogin.Designer.cs
@@ -37,10 +37,10 @@
             this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
             this.btnLogInWIthName = new MetroFramework.Controls.MetroTile();
             this.btnTakePicture = new MetroFramework.Controls.MetroTile();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cameraBox = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnCancel = new MetroFramework.Controls.MetroTile();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,16 +50,18 @@
             // 
             // 
             this.txtVardas.CustomButton.Image = null;
-            this.txtVardas.CustomButton.Location = new System.Drawing.Point(110, 1);
+            this.txtVardas.CustomButton.Location = new System.Drawing.Point(265, 2);
+            this.txtVardas.CustomButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtVardas.CustomButton.Name = "";
-            this.txtVardas.CustomButton.Size = new System.Drawing.Size(16, 17);
+            this.txtVardas.CustomButton.Size = new System.Drawing.Size(31, 28);
             this.txtVardas.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.txtVardas.CustomButton.TabIndex = 1;
             this.txtVardas.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.txtVardas.CustomButton.UseSelectable = true;
             this.txtVardas.CustomButton.Visible = false;
             this.txtVardas.Lines = new string[0];
-            this.txtVardas.Location = new System.Drawing.Point(477, 127);
+            this.txtVardas.Location = new System.Drawing.Point(636, 156);
+            this.txtVardas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtVardas.MaxLength = 32767;
             this.txtVardas.Name = "txtVardas";
             this.txtVardas.PasswordChar = '\0';
@@ -68,7 +70,7 @@
             this.txtVardas.SelectionLength = 0;
             this.txtVardas.SelectionStart = 0;
             this.txtVardas.ShortcutsEnabled = true;
-            this.txtVardas.Size = new System.Drawing.Size(169, 23);
+            this.txtVardas.Size = new System.Drawing.Size(225, 28);
             this.txtVardas.TabIndex = 0;
             this.txtVardas.UseSelectable = true;
             this.txtVardas.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -80,16 +82,18 @@
             // 
             // 
             this.txtPavarde.CustomButton.Image = null;
-            this.txtPavarde.CustomButton.Location = new System.Drawing.Point(110, 1);
+            this.txtPavarde.CustomButton.Location = new System.Drawing.Point(265, 2);
+            this.txtPavarde.CustomButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPavarde.CustomButton.Name = "";
-            this.txtPavarde.CustomButton.Size = new System.Drawing.Size(16, 17);
+            this.txtPavarde.CustomButton.Size = new System.Drawing.Size(31, 28);
             this.txtPavarde.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.txtPavarde.CustomButton.TabIndex = 1;
             this.txtPavarde.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.txtPavarde.CustomButton.UseSelectable = true;
             this.txtPavarde.CustomButton.Visible = false;
             this.txtPavarde.Lines = new string[0];
-            this.txtPavarde.Location = new System.Drawing.Point(477, 156);
+            this.txtPavarde.Location = new System.Drawing.Point(636, 192);
+            this.txtPavarde.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPavarde.MaxLength = 32767;
             this.txtPavarde.Name = "txtPavarde";
             this.txtPavarde.PasswordChar = '\0';
@@ -98,7 +102,7 @@
             this.txtPavarde.SelectionLength = 0;
             this.txtPavarde.SelectionStart = 0;
             this.txtPavarde.ShortcutsEnabled = true;
-            this.txtPavarde.Size = new System.Drawing.Size(169, 23);
+            this.txtPavarde.Size = new System.Drawing.Size(225, 28);
             this.txtPavarde.TabIndex = 0;
             this.txtPavarde.UseSelectable = true;
             this.txtPavarde.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -110,16 +114,18 @@
             // 
             // 
             this.txtSlaptazodis.CustomButton.Image = null;
-            this.txtSlaptazodis.CustomButton.Location = new System.Drawing.Point(110, 1);
+            this.txtSlaptazodis.CustomButton.Location = new System.Drawing.Point(265, 2);
+            this.txtSlaptazodis.CustomButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSlaptazodis.CustomButton.Name = "";
-            this.txtSlaptazodis.CustomButton.Size = new System.Drawing.Size(16, 17);
+            this.txtSlaptazodis.CustomButton.Size = new System.Drawing.Size(31, 28);
             this.txtSlaptazodis.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.txtSlaptazodis.CustomButton.TabIndex = 1;
             this.txtSlaptazodis.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.txtSlaptazodis.CustomButton.UseSelectable = true;
             this.txtSlaptazodis.CustomButton.Visible = false;
             this.txtSlaptazodis.Lines = new string[0];
-            this.txtSlaptazodis.Location = new System.Drawing.Point(477, 185);
+            this.txtSlaptazodis.Location = new System.Drawing.Point(636, 228);
+            this.txtSlaptazodis.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSlaptazodis.MaxLength = 32767;
             this.txtSlaptazodis.Name = "txtSlaptazodis";
             this.txtSlaptazodis.PasswordChar = '*';
@@ -128,7 +134,7 @@
             this.txtSlaptazodis.SelectionLength = 0;
             this.txtSlaptazodis.SelectionStart = 0;
             this.txtSlaptazodis.ShortcutsEnabled = true;
-            this.txtSlaptazodis.Size = new System.Drawing.Size(169, 23);
+            this.txtSlaptazodis.Size = new System.Drawing.Size(225, 28);
             this.txtSlaptazodis.TabIndex = 0;
             this.txtSlaptazodis.UseSelectable = true;
             this.txtSlaptazodis.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -137,45 +143,50 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(393, 131);
+            this.metroLabel1.Location = new System.Drawing.Point(524, 161);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(50, 19);
+            this.metroLabel1.Size = new System.Drawing.Size(52, 20);
             this.metroLabel1.TabIndex = 1;
             this.metroLabel1.Text = "Vardas:";
             // 
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(393, 160);
+            this.metroLabel2.Location = new System.Drawing.Point(524, 197);
+            this.metroLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(59, 19);
+            this.metroLabel2.Size = new System.Drawing.Size(61, 20);
             this.metroLabel2.TabIndex = 1;
             this.metroLabel2.Text = "Pavardė:";
             // 
             // metroLabel3
             // 
             this.metroLabel3.AutoSize = true;
-            this.metroLabel3.Location = new System.Drawing.Point(393, 189);
+            this.metroLabel3.Location = new System.Drawing.Point(524, 233);
+            this.metroLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.metroLabel3.Name = "metroLabel3";
-            this.metroLabel3.Size = new System.Drawing.Size(78, 19);
+            this.metroLabel3.Size = new System.Drawing.Size(80, 20);
             this.metroLabel3.TabIndex = 1;
             this.metroLabel3.Text = "Slaptažodis:";
             // 
             // metroLabel4
             // 
             this.metroLabel4.AutoSize = true;
-            this.metroLabel4.Location = new System.Drawing.Point(41, 0);
+            this.metroLabel4.Location = new System.Drawing.Point(55, 0);
+            this.metroLabel4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.metroLabel4.Name = "metroLabel4";
-            this.metroLabel4.Size = new System.Drawing.Size(401, 19);
+            this.metroLabel4.Size = new System.Drawing.Size(418, 20);
             this.metroLabel4.TabIndex = 2;
             this.metroLabel4.Text = "Prisijunkite naudodami veido atpažinimą arba naudotojo duomenis";
             // 
             // btnLogInWIthName
             // 
             this.btnLogInWIthName.ActiveControl = null;
-            this.btnLogInWIthName.Location = new System.Drawing.Point(393, 236);
+            this.btnLogInWIthName.Location = new System.Drawing.Point(524, 290);
+            this.btnLogInWIthName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogInWIthName.Name = "btnLogInWIthName";
-            this.btnLogInWIthName.Size = new System.Drawing.Size(253, 56);
+            this.btnLogInWIthName.Size = new System.Drawing.Size(337, 69);
             this.btnLogInWIthName.TabIndex = 5;
             this.btnLogInWIthName.Text = "Prisijungti";
             this.btnLogInWIthName.UseSelectable = true;
@@ -184,32 +195,35 @@
             // btnTakePicture
             // 
             this.btnTakePicture.ActiveControl = null;
-            this.btnTakePicture.Location = new System.Drawing.Point(41, 236);
+            this.btnTakePicture.Location = new System.Drawing.Point(55, 290);
+            this.btnTakePicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnTakePicture.Name = "btnTakePicture";
-            this.btnTakePicture.Size = new System.Drawing.Size(234, 56);
+            this.btnTakePicture.Size = new System.Drawing.Size(312, 69);
             this.btnTakePicture.TabIndex = 5;
             this.btnTakePicture.Text = "Atpažinti veidą";
             this.btnTakePicture.UseSelectable = true;
             this.btnTakePicture.Click += new System.EventHandler(this.btnTakePicture_Click);
             // 
-            // pictureBox2
+            // cameraBox
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.BackgroundImage = global::Virtual_librarian.Properties.Resources.avatar;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(41, 66);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(234, 163);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
+            this.cameraBox.BackColor = System.Drawing.Color.Transparent;
+            this.cameraBox.BackgroundImage = global::Virtual_librarian.Properties.Resources.avatar;
+            this.cameraBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cameraBox.Location = new System.Drawing.Point(55, 81);
+            this.cameraBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(312, 201);
+            this.cameraBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cameraBox.TabIndex = 4;
+            this.cameraBox.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Virtual_librarian.Properties.Resources.gray_line_png1;
-            this.pictureBox1.Location = new System.Drawing.Point(299, 42);
+            this.pictureBox1.Location = new System.Drawing.Point(399, 52);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(88, 276);
+            this.pictureBox1.Size = new System.Drawing.Size(117, 340);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -217,10 +231,10 @@
             // btnCancel
             // 
             this.btnCancel.ActiveControl = null;
-            this.btnCancel.Location = new System.Drawing.Point(535, 2);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancel.Location = new System.Drawing.Point(713, 2);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(146, 38);
+            this.btnCancel.Size = new System.Drawing.Size(195, 47);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Atšaukti";
             this.btnCancel.UseSelectable = true;
@@ -228,12 +242,12 @@
             // 
             // UCLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnTakePicture);
             this.Controls.Add(this.btnLogInWIthName);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.cameraBox);
             this.Controls.Add(this.metroLabel4);
             this.Controls.Add(this.metroLabel3);
             this.Controls.Add(this.metroLabel2);
@@ -242,9 +256,10 @@
             this.Controls.Add(this.txtPavarde);
             this.Controls.Add(this.txtVardas);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UCLogin";
-            this.Size = new System.Drawing.Size(683, 345);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Size = new System.Drawing.Size(911, 425);
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -261,7 +276,7 @@
         private MetroFramework.Controls.MetroLabel metroLabel3;
         private MetroFramework.Controls.MetroLabel metroLabel4;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox cameraBox;
         private MetroFramework.Controls.MetroTile btnLogInWIthName;
         private MetroFramework.Controls.MetroTile btnTakePicture;
         private MetroFramework.Controls.MetroTile btnCancel;
--- Virtual_librarian/Virtual_librarian/UCLogin.cs
+++ Virtual_librarian/Virtual_librarian/UCLogin.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using System.Reflection;
+using System.IO;
 
 
 namespace Virtual_librarian
@@ -16,46 +18,97 @@ namespace Virtual_librarian
     public partial class UCLogin : MetroFramework.Controls.MetroUserControl
     {
         private MainForm mainForm;
-        private VideoCapture capture;
-        Mat m;
+        //Declare variables
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, byte> Frame;
+        Capture camera;
+
+        Image<Gray, byte> result;
+        Image<Gray, byte> trainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<String> labels = new List<String>();
+        List<String> Users = new List<String>();
+        int Count, NumLables, t;
+        String name, names = null;
+
 
         public UCLogin(MainForm mainForma)
         {
             InitializeComponent();
             mainForm = mainForma;
+            //HarrCascade is for face detection
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"x86\haarcascade_frontalface_default.xml");
+
+            faceDetected = new HaarCascade(path);
+            try
+            {
+                string LabelsInf = File.ReadAllText(Application.StartupPath + "Faces/Faces.txt");
+                string[] Labels = LabelsInf.Split(',');
+                //The first label before , will be the number of faces saved
+                NumLables = Convert.ToInt16(Labels[0]);
+                Count = NumLables;
+                string FaceLoad;
+                for (int i = 1; i < NumLables + 1; i++)
+                {
+                    FaceLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nothing in the database");
+            }
         }
 
         private void btnTakePicture_Click(object sender, EventArgs e)
         {
-            if (capture == null)
+            if (camera == null)
             {
-                capture = new VideoCapture(0);
+                camera = new Capture(0);
 
             }
-            capture.ImageGrabbed += Capture_ImageGrabbed1;
-            capture.Start();
-            //Veido atpažinimo kodas
+            camera.Start();
+            Application.Idle += new EventHandler(FrameProcedure);
         }
-        private void Capture_ImageGrabbed1(object sender, EventArgs e)
+        private void FrameProcedure(object sender, EventArgs e)
         {
-            try
+            if (camera != null)
             {
+                Users.Add("");
+                Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                grayFace = Frame.Convert<Gray, Byte>();
+                MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+                foreach (MCvAvgComp f in facesDetectedNow[0])
+                {
+                    result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                    if (trainingImages.ToArray().Length != 0)
+                    {
+                        MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.001);
+                        EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                        //name = recognizer.Recognize(result);
+                        //Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
 
-                m = new Mat();
-                capture.Retrieve(m);
-                pictureBox2.Image = m.ToImage<Bgr, byte>().ToBitmap();
+                    }
+                    //Users[t - 1] = name;
+                    Users.Add("");
+                }
+                cameraBox.Image = Frame.ToBitmap();
+                names = "";
+                Users.Clear();
             }
-            catch (Exception)
-            {
 
-            }
+
         }
         private void btnLogInWIthName_Click(object sender, EventArgs e)
         {
             Zmogus prisijungesZmogus = mainForm.humanDBHelper.getZmogusByNameSurnamePassword(txtVardas.Text, txtPavarde.Text, txtSlaptazodis.Text);
-            if (capture != null)
+            if (camera != null)
             {
-                capture.Dispose();
+                camera.Dispose();
             }
             if (prisijungesZmogus != null)
             {
@@ -69,9 +122,9 @@ namespace Virtual_librarian
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            if (capture != null)
+            if (camera != null)
             {
-                capture.Dispose();
+                camera.Dispose();
             }
             UCChooseLogin ucChooseLogin = new UCChooseLogin(mainForm);
             ucChooseLogin.Dock = DockStyle.Bottom;
