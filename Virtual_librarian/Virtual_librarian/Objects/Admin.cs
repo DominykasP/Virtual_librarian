94d55909733b45c9a584e340d4e15a6e53495e4c
CheskaG <domantasgabalis@gmail.com>
2018-10-17 13:51:31 +0300

    Created Admin derrived class

---
+++ Virtual_librarian/Virtual_librarian/Objects/Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_librarian.Objects
+{
+    public class Admin : Zmogus
+    {
+        private Enum Job;
+
+        public Admin(string name, string surname, string password, DateTime birthdate, string phoneNumber, string email,Enum job)
+        {
+            Name = name;
+            Surname = surname;
+            Password = password;
+            Birthdate = birthdate;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Job = job;
+        }
+        public string Job{ get => job; set => job = value; }
+
+    }
+
+    public enum job { job1, job2, job3 };
+}
