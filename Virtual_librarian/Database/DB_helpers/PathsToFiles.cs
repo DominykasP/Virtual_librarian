990917ba4a629ee2a163fb5f89e63efd49678628
entorius <eligijus.puplinskas@gmail.com>
2018-11-06 16:35:20 +0200

    faces folder added to database project

--- Virtual_librarian/Database/DB_helpers/PathsToFiles.cs
+++ Virtual_librarian/Database/DB_helpers/PathsToFiles.cs
@@ -8,8 +8,8 @@ namespace Database
 {
     public static class PathsToFiles
     {
-        public static string pathToFacesFile = "..\\..\\Faces\\Faces.xml";
-        public static string pathToFacesFolder = "..\\..\\Faces\\";
+        public static string pathToFacesFile = @"..\..\..\Database\Faces\Faces.xml";
+        public static string pathToFacesFolder = @"..\..\..\Database\Faces\";
         public static string pathToUsersFile = @"..\..\..\Database\Data_Files\users.xml";
         public static string pathToBooksFile = @"..\..\..\Database\Data_Files\books.xml";
     }
