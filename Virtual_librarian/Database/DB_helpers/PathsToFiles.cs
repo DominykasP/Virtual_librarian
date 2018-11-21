15725613ac18a00f6460c7df2400af894e453d74
entorius <eligijus.puplinskas@gmail.com>
2018-11-21 13:02:34 +0200

    Pakeisit paths i Eligijaus kompa

--- Virtual_librarian/Database/DB_helpers/PathsToFiles.cs
+++ Virtual_librarian/Database/DB_helpers/PathsToFiles.cs
@@ -8,9 +8,9 @@ namespace Database
 {
     public static class PathsToFiles
     {
-        public static string pathToFacesFile = @"C:\Users\domin\Documents\Virtualaus biblietokininko failai\Virtual_librarian\Virtual_librarian\Database\Faces\Faces.xml";
-        public static string pathToFacesFolder = @"C:\Users\domin\Documents\Virtualaus biblietokininko failai\Virtual_librarian\Virtual_librarian\Database\Faces\";
-        public static string pathToUsersFile = @"C:\Users\domin\Documents\Virtualaus biblietokininko failai\Virtual_librarian\Virtual_librarian\Database\Data_Files\users.xml";
-        public static string pathToBooksFile = @"C:\Users\domin\Documents\Virtualaus biblietokininko failai\Virtual_librarian\Virtual_librarian\Database\Data_Files\books.xml";
+        public static string pathToFacesFile = @"C:\Users\ELIGIJUS\git\Virtual_librarian\Virtual_librarian\Database\Faces\Faces.xml";
+        public static string pathToFacesFolder = @"C:\Users\ELIGIJUS\git\Virtual_librarian\Virtual_librarian\Database\Faces\";
+        public static string pathToUsersFile = @"C:\Users\ELIGIJUS\git\Virtual_librarian\Virtual_librarian\Database\Data_Files\users.xml";
+        public static string pathToBooksFile = @"C:\Users\ELIGIJUS\git\Virtual_librarian\Virtual_librarian\Database\Data_Files\books.xml";
     }
 }
